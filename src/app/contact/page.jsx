eadfba6314d9a2e399a3792e483a7637edd18589
Furkan Coşar <furkancosar2005@gmail.com>
2026-05-03 02:07:31 +0300

    Add marquee banners

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,12 @@
 import ContactHero from "@/components/Contact/ContactHero/ContactHero";
+import MarqueeText from "@/components/MarqueeText/MarqueeText";
 import ContactForm from "@/components/Contact/ContactForm/ContactForm";
 
 export default function ContactPage() {
   return (
     <main>
       <ContactHero />
+      <MarqueeText text="LET&apos;S TALK" duration="50s" />
       <ContactForm />
     </main>
   );
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,12 +1,14 @@
-import CollaborationCTA from "@/components/CollaborationCTA/CollaborationCTA";
 import ProjectsHero from "@/components/Projects/ProjectsHero/ProjectsHero";
+import MarqueeText from "@/components/MarqueeText/MarqueeText";
 import ProjectsIntro from "@/components/Projects/ProjectsIntro/ProjectsIntro";
 import ProjectsList from "@/components/Projects/ProjectsList/ProjectsList";
+import CollaborationCTA from "@/components/CollaborationCTA/CollaborationCTA";
 
 export default function ProjectsPage() {
   return (
     <main>
       <ProjectsHero />
+      <MarqueeText text="MY PROJECTS" duration="50s" />
       <ProjectsIntro />
       <ProjectsList />
       <CollaborationCTA />
